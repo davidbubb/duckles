396f1644719739fc23c9d9367e68e0276f1e9efc
David Bubb <David.Bubb@cityandguilds.com>
2026-02-07 09:26:07 +0000

    Set up github pages

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/duckles/',
   server: {
     port: 3000,
     host: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 })
